b234c34c99ba03ce82a772385a526241ee9415e9
maikwuerth <maikwuerth@users.noreply.github.com>
2021-05-11 13:41:46 +0200

    Update feed.php
    teilweise beitrag html integriert

--- feed.php
+++ feed.php
@@ -24,12 +24,26 @@ if ($conn->connect_error) {
 
 //hole daten
 $posts = $conn->query($sql);
-
-//zeige daten
-foreach($posts as $post) {
-  //MAX HTML CODE für einzelne posts
-}
-
+if ($posts->num_rows > 0) {
+  while($post = $posts->fetch_assoc()) {
+    //zeige daten
+    <ul>
+    //MAX HTML CODE für einzelne posts
+    <li>
+      <img src="Bilder/avatar.PNG" alt="Avatar">
+      <div class="beitrag">
+        <strong>$post["user"] <span>@daddy69</span></strong>
+        <p>$post["text"]</p>
+        <div class="actions">
+          <a href=""><alt="kommentieren">Kommentieren</alt="kommentieren"></a>
+          <a href=""><alt="zwitschern">Zwitschern</alt="zwitschern"></a>
+          <a href=""><alt="gefällt mir">Gefällt mir</alt="gefällt"></a>
+        </div>
+      </div>
+    </li>
+    }
+    </ul>
+  }
 // ende
 $conn->close();
 ?> 
