73a7b42b16808b169f495f51b4cdf24b9b5e0537
Masci24 <77623122+Masci24@users.noreply.github.com>
2021-05-12 14:17:10 +0200

    Add files via upload

--- Insert_data.php
+++ Insert_data.php
@@ -1,13 +1,13 @@
 <?php
 
 function new_quak_insert($Titel, $Quak_ID, $Benutzer_ID, $Inhalt) {
- $sql = "INSERT INTO Quaks (Titel, Quak_ID, Benutzer_ID, Inhalt)
+ $sql = "INSERT INTO Quaks (Tags, Quak_ID, Benutzer_ID, Inhalt)
  VALUES ('$Titel', '$Quak_ID', '$Benutzer_ID', '$Inhalt')";
  return $sql;
 }
 
 
-
+$Inhalt = $_POST["inhalt"];
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -21,7 +21,11 @@ $conn =new mysqli($servername, $username, $password, $dbname);
  if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
  }
- $sql=new_quak_insert("Test", 001, 001, "test12345");
+ // Ermittle neue Quak_ID
+ $Quak_ID = $conn->query("SELECT Quak_ID From Quaks ORDER BY Quak_ID DESC LIMIT 1");
+ echo $Quak_ID;
+ //Neuen Quak in DB einfügen
+ $sql = new_quak_insert("Test", $Quak_ID, 001, $Inhalt);
  
  if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
